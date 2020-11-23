a06f8b81e60c3f3f3080cfd1de7a1cc8c3c7a575
Pufelu <62748809+Pufelu@users.noreply.github.com>
2020-11-23 20:26:48 +0200

    testing values for those blurs -.-

--- Detectie/ACPI/PlateDetection.cpp
+++ Detectie/ACPI/PlateDetection.cpp
@@ -1,22 +1,39 @@
 #include <QApplication>
 #include "ImageGrid.h"
 #include "PlateDetection.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-int main(int argc, char *argv[])
-{	
-	QApplication a(argc, argv);
+#include <iostream>
 
-	ImageGrid* grid = new ImageGrid("Prelucrarea imaginilor");
+using namespace cv;
 
-	QString imageDir = "Images/";
-	QString imageFile(imageDir + "lena512.bmp");
+int main()
+{
+	Mat img = imread("Images/test.jpg", IMREAD_GRAYSCALE);
+	Mat eqh, sobel, sobeleqh, gaussian, morph;
+	equalizeHist(img, eqh);
 
-	grid->addImage(imageFile, 0, 0);
+	Sobel(img, sobel, img.depth(), 1, 0, 3);
+	Sobel(eqh, sobeleqh, eqh.depth(), 1, 0, 3);
 
-	int w, h;
-	unsigned char* img = Tools::readImageGray8(imageFile, w, h);
+	GaussianBlur(sobeleqh, gaussian, Size(11, 11), 10, 3);
+	imshow("GaussianBlur", gaussian);
 
-	grid->show();
-	return a.exec();
+	threshold(gaussian, gaussian, 30, 255, THRESH_BINARY);
+
+	auto kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+	morphologyEx(gaussian, morph, MORPH_OPEN, kernel, Point(-1, -1), 1);
+	morphologyEx(morph, morph, MORPH_CLOSE, kernel, Point(-1, -1), 1);
+
+
+	//imshow("Asa de test", img);
+	//imshow("Histograma", eqh);
+	//imshow("Sobel", sobel);
+	//imshow("SobelEqh", sobeleqh);
+	imshow("Binarization", gaussian);
+	imshow("Morphology", morph);
+	waitKey();
+	return 0;
 }
-
